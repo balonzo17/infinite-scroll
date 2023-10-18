4f1c76b55ee62ae72cc63fb5cd7e22684a782b16
balonzo17 <alonzo.code17@gmail.com>
2023-10-18 15:04:19 -0400

    unfinished fetch request for api

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Unsplash API
+const count = 10;
+const apiKey = 'SWwcgMbEyWdrJCbuV58zXy9PztK39LdyPKoS81tBCNE';
+const querySearch = ''
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// get photos from Unsplash API
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
