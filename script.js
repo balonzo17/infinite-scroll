1a0ccc4d6d37b08c10c126bedb934f440249c584
balonzo17 <alonzo.code17@gmail.com>
2023-10-21 01:08:27 -0400

    javascript display data

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const count = 10;
 const apiKey = 'SWwcgMbEyWdrJCbuV58zXy9PztK39LdyPKoS81tBCNE';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// // Helper Function to Set Attribute on DOM Elements
+// function setAttribute(element,attributes) {
+//     for (const key in attributes) {
+//         element.setAttribute(key, attributes[key]);
+//     }
+// }
+
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
     // Run Function for each object in photosArray
@@ -16,12 +23,22 @@ function displayPhotos() {
         const item = document.createElement('a');
         item.setAttribute('href', photo.links.html);
         item.setAttribute('target', '_blank');
+        // setAttributes(item, {
+        //     href: photo.links.html,
+        //     target: '_blank',
+        // });
+
         // Create <img> for photo
         const img = document.createElement('img');
         img.setAttribute('src', photo.urls.regular);
         img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
-        // Put <img> inside <a>, then put both inside imageConatiner Element
+        img.setAttribute('title', photo.alt_descripttion);
+        // setAttributes(img, {
+        //    src: photo.urls.regular, 
+        //    alt: photo.alt_description,
+        //    title: photo.alt_description,
+        // });
+
         item.appendChild(img)
         imageContainer.appendChild(item);
     });
